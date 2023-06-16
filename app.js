18557a4c2b2aa99284a5ee14a63608a350d6420b
Kyaw Thu <evanch98@gmail.com>
2023-06-17 00:19:05 +0800

    update the createBoard function

--- app.js
+++ app.js
@@ -77,10 +77,14 @@ const startPieces = [
 function createBoard() {
   // there are total of 64 pieces including the empty spaces
   // for each piece create a div
-  startPieces.forEach((startPiece) => {
+  startPieces.forEach((startPiece, i) => {
     const square = document.createElement("div");
     // add the classList
     square.classList.add("square");
+    // change the innerHTML of the div to startPiece
+    square.innerHTML = startPiece;
+    // give an unique attribute to each square
+    square.setAttribute("square-id", i);
     square.classList.add("beige");
     // add to the gameboard
     gameBoard.append(square);
