27c16f1e0b947735f4cf629a3087baefda948c2a
Kyaw Thu <evanch98@gmail.com>
2023-06-16 23:45:45 +0800

    retrieve the document by id

---
+++ app.js
@@ -0,0 +1,4 @@
+/* Retrieving the document */
+const gameBoard = document.querySelector("#gameboard");
+const playerDisplay = document.querySelector("#player");
+const infoDisplay = document.querySelector("#info-display");
