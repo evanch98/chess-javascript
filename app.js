123ba11592de490f21401d10aa1a37a2135e820f
Kyaw Thu <evanch98@gmail.com>
2023-06-16 23:47:51 +0800

    define the width of the chess board

--- app.js
+++ app.js
@@ -2,3 +2,5 @@
 const gameBoard = document.querySelector("#gameboard");
 const playerDisplay = document.querySelector("#player");
 const infoDisplay = document.querySelector("#info-display");
+
+const width = 8; // a chess board is made up of 8x8 tiles
