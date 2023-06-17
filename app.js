bee91d309838f2bc82b22c0a544249a850198715
Kyaw Thu <evanch98@gmail.com>
2023-06-17 21:22:58 +0800

    fix bug

--- app.js
+++ app.js
@@ -208,14 +208,14 @@ function revertIds() {
 // to check if a player has won
 function checkForWin() {
   const kings = Array.from(document.querySelectorAll("#king"));
-  if (!kings.some((king) => king.firstChild.contains("white"))) {
+  if (!kings.some((king) => king.firstChild.classList.contains("white"))) {
     infoDisplay.innerHTML = "Black player wins!";
     const allSquares = document.querySelectorAll(".square");
     allSquares.forEach((square) =>
       square.firstChild?.setAttribute("draggable", false)
     );
   }
-  if (!kings.some((king) => king.firstChild.contains("black"))) {
+  if (!kings.some((king) => king.firstChild.classList.contains("black"))) {
     infoDisplay.innerHTML = "White player wins!";
     const allSquares = document.querySelectorAll(".square");
     allSquares.forEach((square) =>
